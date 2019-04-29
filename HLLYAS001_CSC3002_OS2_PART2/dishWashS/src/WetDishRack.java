45c6065a8e1d100c554b6043e549737bdae9f611
yaseenhull <yaseenhull@gmail.com>
2019-04-29 19:22:08 +0200

    added comments

--- HLLYAS001_CSC3002_OS2_PART2/dishWashS/src/WetDishRack.java
+++ HLLYAS001_CSC3002_OS2_PART2/dishWashS/src/WetDishRack.java
@@ -10,16 +10,8 @@ public class WetDishRack {
 	// add variables
 	int rackSize;
 	int count;
-	private int rack[];
 	LinkedList buffer = new LinkedList(); //rack structure: finite buffer
-	int top = -1;
 	
-	//Semaphore mutexCount = new Semaphore(1); //count:provides exclusive access to threads by decrementing (acquiring:0 (blocking)) and incrementing (releasing:1 (unblocking))
-	//Semaphore mutexUser = new Semaphore(1);
-	//Semaphore barrier = new Semaphore(0); // <= 0 blocks threads, > 0 unblocks threads allowing them to pass through
-	
-	//Semaphore washer = new Semaphore(0);
-	//Semaphore dryer = new Semaphore(1);
 	Semaphore mutex = new Semaphore(1); // provides exclusive access to threads by decrementing (acquiring:0 (blocking)) and incrementing (releasing:1 (unblocking))
 	Semaphore dishCount = new Semaphore(0); // keeps track of number of dishes in rack
 	Semaphore openRackSlots; // keeps track of available spaces in rack
@@ -28,7 +20,7 @@ public class WetDishRack {
 	WetDishRack(int rackSize) {
 	    // add correct code here 
 	    this.rackSize = rackSize;
-	    //this.rack =  new int[rackSize];
+	
 	    this.openRackSlots = new Semaphore(rackSize); // creates a semaphore with rackSize permits
 	}		
 	
@@ -56,18 +48,15 @@ public class WetDishRack {
 		dishCount.acquire(); // decrements number items on rack: take dish off rack
 		
 		mutex.acquire();
-			//count = rack[top--];
+		
 			count = (Integer)buffer.remove(0); // remove dish from rack
 		mutex.release();
 			
 		openRackSlots.release();	//increments no. of available slots: free up space on rack
 		
-		return count; // replace with correct code here
+		return count; 
 		// if dishes are clean dry and remove
-		// if dishes != rack size; wait - barrier implementation
-		// when dishes == rack size; release()
-		// if barrier.Semaphore <= 0 block (acquire)
-		// when the rack is empty dryer must wait translates to if barrier !> 0
+		
 	
 	}
 	
